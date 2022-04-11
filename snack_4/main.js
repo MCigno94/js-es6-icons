af08b195400424c33abf974d9854d3a711dfc480
MCigno94 <mattiacigno27@gmail.com>
2022-04-11 16:58:25 +0200

    added comments

--- snack_4/main.js
+++ snack_4/main.js
@@ -47,17 +47,33 @@ const listPerson = [{
 
 ]
 
+
+/* Creo un nuovo array inserendo, per ogni persona, 
+una frase con il nome e cognome e l’indicazione se può guidare, 
+in base all’età.*/
+
 const listAdultPerson = listPerson.map(canDrive);
 
+/**
+ * @param {string} item seleziona un elemento dell'array
+ * @param {number} i indica la posizione degli oggetti
+ * @param {array} array nuovo array creato
+ * @returns array con una nuova proprietà in più
+ */
 function canDrive(item, i, array) {
     if (item.eta >= 18) {
         array[i]['patente B'] = 'si'
+            //dichiaro se person può guidare
+        console.log(`${item.nome} ${item.cognome} ha ${item.eta} anni e può guidare`);
         return array[i];
     } else {
         array[i]['patente B'] = 'no';
+        //dichiaro se person non può guidare
+        console.log(`${item.nome} ${item.cognome} ha ${item.eta} anni e non può guidare`);
         return array[i];
     }
 
 }
 
+//stampo il nuovo array
 console.log(listAdultPerson);
